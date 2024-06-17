ab4e97ce697b42d40f8fb19c05989cab043da2b7
hungnnhe173202 <hungngongocfpt@gmail.com>
2024-06-17 10:44:26 +0700

    add first project

---
+++ src/main/java/com/miniApartment/miniApartment/MiniApartmentApplication.java
@@ -0,0 +1,13 @@
+package com.miniApartment.miniApartment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniApartmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniApartmentApplication.class, args);
+	}
+
+}
