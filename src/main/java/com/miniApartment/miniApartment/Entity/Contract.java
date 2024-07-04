710bc550347f56bd99164e221e8d5c0eed0384c2
hungnnhe173202 <hungngongocfpt@gmail.com>
2024-07-04 10:09:31 +0700

    fixed forgetPassword

--- src/main/java/com/miniApartment/miniApartment/Entity/Contract.java
+++ src/main/java/com/miniApartment/miniApartment/Entity/Contract.java
@@ -7,6 +7,7 @@ import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.util.Date;
+import java.util.List;
 
 @Data
 @Getter
@@ -42,4 +43,5 @@ public class Contract {
         this.expireDate = expireDate;
         this.contractStatus = contractStatus;
     }
+
 }
