fd142b85fdf5382b66d3840073b9b4e29422e54e
minhtt <minhtthe273206@fpt.edu.vn>
2024-07-30 09:22:02 +0700

    add swagger

---
+++ src/main/java/com/miniApartment/miniApartment/Security/OpenApiConfig.java
@@ -0,0 +1,19 @@
+package com.miniApartment.miniApartment.Security;
+
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@Configuration
+@CrossOrigin
+public class OpenApiConfig {
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public")
+                .pathsToMatch("/**")
+                .build();
+    }
+}
